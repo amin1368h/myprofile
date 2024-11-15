4433154582031731f40dcc498485d9e33551901f
amin1368h <amin6813h@gmail.com>
2024-11-16 02:29:36 +0330

    mywebsite

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let imgProfile=document.getElementById('img')
+let menu=document.querySelector('.menu')
+let exit=document.getElementById('exit')
+
+imgProfile.addEventListener('click',function(){
+     menu.classList.toggle('active')
+}) 
+
+
